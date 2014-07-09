5b6e020c3fcd87675c4b702f2b71a73e8d3c789c
Klaus Ahrens <ahrens@informatik.hu-berlin.de>
2014-07-09 12:01:58 +0200

    Initial Commit

---
+++ philos/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  philos
+//
+//  Created by Klaus Ahrens on 09.07.14.
+//  Copyright (c) 2014 Klaus Ahrens. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
