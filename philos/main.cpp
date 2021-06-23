51057836e3db592579c31eb9a8798f8872618272
fidus58 <ahrens@informatik.hu-berlin.de>
2021-06-23 11:04:05 +0200

    nicht ganz wie im Skript

--- philos/main.cpp
+++ philos/main.cpp
@@ -6,13 +6,35 @@
 //  Copyright (c) 2014 Klaus Ahrens. All rights reserved.
 //
 
+#include <thread>
+#include <vector>
+#include <chrono>
 #include <iostream>
 
-int main(int argc, const char * argv[])
-{
+std::mutex forks[5];
 
-    // insert code here...
-    std::cout << "Hello, World!\n";
+void philo(int i) {
+    for (int n=0; n<10;++n) {
+        forks[   i   ].lock();
+        std::cout<<"fork "<<i<<" locked\n";
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        forks[(i+1)%5].lock();
+        std::cout<<"fork "<<(i+1)%5<<" locked\n";
+
+        std::cout<<"philosopher "<<i<<" is eating "
+                 <<"["<<n<<"]\n";
+        forks[(i+1)%5].unlock();
+        forks[   i   ].unlock();
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+}
+std::vector<std::thread> philos;
+
+int main(int argc, const char * argv[]) {
+    for (int i=0; i<5; ++i) {
+        std::thread th(philo, i);
+        philos.push_back(std::move(th));
+    }
+    std::for_each(begin(philos), end(philos),[](std::thread& t) {t.join();});
     return 0;
 }
-
